c0a582d6bbd37d75c9dd6bf8ae69f2491b4e18f7
Monetthigpen <monet.thigpen@gmail.com>
2019-02-11 23:19:58 -0500

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,96 +1,125 @@
 require("dotenv").config();
-var inquirer =  require("inquirer");
+//var inquirer =  require("inquirer");
 var password = require("./key");
+// Initializes the npm packages used
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
 
+// Initializes the connection variable to sync with a MySQL database
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306 ,
-  
-    // Your username
-    user: "root",
-  
-    // Your paback8987ssword
-    password: process.env.password,
-    database: "bamazon_db"
-});
-//  connection.connect(function(err) {
-//     //if (err) throw err;
-//     console.log("connected as id " + connection.threadId);
-//     findItems();
-// });
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId);
-//     connection.query("SELECT * FROM products", function(err, res){
-//       if  (err) throw err;
-//       console.log(res);
-//     });
-//     findItems();
-//     //connection.end();
-//   });
-findItems();
-function findItems() {
-    connection.connect(function(err) {
-        if (err) throw err;
-        console.log("connected as id " + connection.threadId);
-        connection.query("SELECT * FROM products", function(err, res){
-          if  (err) throw err;
-          console.log(res);
-        
-        findItems();
-        //connection.end();
-      
-    inquirer
-    .prompt([
-         {
-            name: "id",
-            type: "input",
-            message: "What is the ID number of the product you would  like to buy?"
-        },{
-             name: "units",
-             type: "input",
-             message: "How many units of the product would you like to buy?"
-        
-        }
-    ]).then(function(inquirerResponse){
-        var chosenItem;
-        for (var i = 0; i < res.length; i++) {
-          if (results[i].item_id === inquirerResponse.id) {
-            chosenItem = res[i];
-          }
-        }
+  host: "localhost",
 
-        // determine if bid was high enough
-        if (chosenItem.stock_quantity < parseInt(inquirerResponse.units)) {
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE auctions SET ? WHERE ?",
-            [
-              {
-                stock_quantity: inquirerResponse.units
-              },
-              {
-                id: chosenItem.id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("Order placed successfully!");
-              
-            }
-          );
-        }
-        else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Insufficent quantity!");
-          
-        }
-      
-    
-    })
-    })
-   })
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: process.env.password,
+  database: "bamazon_db"
+});
+
+// Creates the connection with the server and loads the product data upon a successful connection
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  loadProducts();
+});
+
+// Function to load the products table from the database and print results to the console
+function loadProducts() {
+  // Selects all of the data from the MySQL products table
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    // Draw the table in the terminal using the response
+    console.table(res);
+
+    // Then prompt the customer for their choice of product, pass all the products to promptCustomerForItem
+    promptCustomerForItem(res);
+  });
 }
+
+// Prompt the customer for a product ID
+function promptCustomerForItem(inventory) {
+  // Prompts user for what they would like to purchase
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "What is the ID of the item you would you like to purchase?",
+        // validate: function(val) {
+        //   return !isNaN(val) || val.toLowerCase() === "q";
+        // }
+      },{
+        name: "units",
+        type: "input",
+        message: "How many units of the product would you like to buy?"
+   
+   }
+    ])
+    .then(function(input) {
+      // Check if the user wants to quit the program
+      //checkIfShouldExit(val.choice);
+      var choiceId = parseInt(input.choice);
+      console.log(choiceId);
+      var product = checkInventory(choiceId, inventory);
+      //console.log(inventory);
+      var quantity = parseInt(input.units);
+      console.log(quantity);
+
+      // If there is a product with the id the user chose, prompt the customer for a desired quantity
+      if (product) {
+        // Pass the chosen product to promptCustomerForQuantity
+        //promptCustomerForQuantity(product);
+      }
+      else {
+        // Otherwise let them know the item is not in the inventory, re-run loadProducts
+        console.log("\nThat item is not in the inventory.");
+        loadProducts();
+      }
+      if (quantity > product.stock_quantity) {
+          console.log("\nInsufficient quantity!");
+            loadProducts();
+      }
+      else {
+                 // Otherwise run makePurchase, give it the product information and desired quantity to purchase
+         makePurchase(product, quantity);
+      }
+            
+    });
+}
+
+
+function makePurchase(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, product.item_id],
+    function(err, res) {
+      // Let the user know the purchase was successful, re-run loadProducts
+      console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
+      loadProducts();
+    }
+  );
+}
+
+// Check to see if the product the user chose exists in the inventory
+function checkInventory(choiceId, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].id === choiceId) {
+      // If a matching product is found, return the product
+      return inventory[i];
+    }
+  }
+  // Otherwise return null
+  return null;
+}
+
+
+
+
+
